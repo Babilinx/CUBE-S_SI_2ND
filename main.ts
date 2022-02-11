b5a8f5a59e16de7ceae2597c3420eecef83b0a28
Babilinx <77548264+Babilinx@users.noreply.github.com>
2022-02-11 17:13:24 +0100

    départ du projet

--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
-
+let Temperature = 0
+loops.everyInterval(10000, function () {
+    Temperature = input.temperature()
+    if (Temperature > 21) {
+        led.plot(3, 3)
+    } else {
+        led.unplot(3, 3)
+    }
+})
+basic.forever(function () {
+	
+})
