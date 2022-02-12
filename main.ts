7672c7764560257453826821d7361cdfa829c38f
Babilinx <77548264+Babilinx@users.noreply.github.com>
2022-02-12 11:49:01 +0100

    ajout led de statut

--- main.ts
+++ main.ts
@@ -1,4 +1,11 @@
 let Temperature = 0
+// indicateur de fonctionement
+loops.everyInterval(10000, function () {
+    for (let index = 0; index < 2; index++) {
+        led.toggle(0, 0)
+        basic.pause(100)
+    }
+})
 loops.everyInterval(10000, function () {
     Temperature = input.temperature()
     if (Temperature > 21) {
