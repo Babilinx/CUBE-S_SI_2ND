24ba9d07fc82e195579924daa3e9e2be5fc8e34b
Bastien <77548264+Babilinx@users.noreply.github.com>
2022-05-06 09:25:54 +0200

    update syntax

--- main.py
+++ main.py
@@ -1,63 +1,55 @@
-def on_received_string(receivedString):
-    global NPersonnes
-    NPersonnes = receivedString
-radio.on_received_string(on_received_string)
-
-NPersonnes = ""
+# Init radio transmition
 radio.set_group(1)
+# Init LCD_Display
 LCD1IN8.LCD_Init()
 LCD1IN8.LCD_Clear()
 LCD1IN8.LCD_SetBL(10)
 LCD1IN8.LCD_Display()
+# Init vaiables
 minute = 34
 heure = 8
-NPersonnes = convert_to_text(0)
+NPersonnes = 0
+
+# Init functions
+def on_received_number(receivedNumber):
+    global NPersonnes
+    NPersonnes = receivedNumber
+    for index in range(2):
+        led.toggle(0, 0)
+        basic.pause(300)
 
 def on_every_interval():
     global minute, heure
     minute += 1
     if minute == 60:
         heure += 1
-loops.every_interval(50000, on_every_interval)
+        minute = 0
+    if heure == 24:
+        heure = 0
 
 def on_every_interval2():
     LCD1IN8.LCD_Clear()
-    # Affichage de la temperature
-    LCD1IN8.dis_string(10,
-        55,
-        "" + convert_to_text(input.temperature()) + " C",
-        LCD1IN8.Get_Color(LCD_COLOR.BLACK))
-    if input.temperature() < 19:
-        LCD1IN8.draw_point(45,
-            62,
-            LCD1IN8.Get_Color(LCD_COLOR.RED),
-            DOT_PIXEL.DOT_PIXEL_4)
+    
+    # Affichage de la temperature.
+    LCD1IN8.dis_string(10, 55, "" + convert_to_text(input.temperature()) + " C", LCD1IN8.Get_Color(LCD_COLOR.BLACK))
+    if input.temperature() < 19:  # si 'temperature' < 19°C, afficher un témoin de chauffe.
+        LCD1IN8.draw_point(45, 62, LCD1IN8.Get_Color(LCD_COLOR.RED), DOT_PIXEL.DOT_PIXEL_4)
+    
     # Nombre de personnes dans la salle
-    LCD1IN8.dis_string(63,
-        15,
-        "" + convert_to_text(NPersonnes) + " Personnes",
-        LCD1IN8.Get_Color(LCD_COLOR.BLACK))
-    # Affichage de la temperature
-    LCD1IN8.dis_string(63,
-        101,
-        "" + convert_to_text(input.light_level()) + "Lux",
-        LCD1IN8.Get_Color(LCD_COLOR.BLACK))
-    LCD1IN8.draw_rectangle(108,
-        104,
-        113,
-        109,
-        LCD1IN8.Get_Color(LCD_COLOR.BLACK),
-        DRAW_FILL.DRAW_EMPTY,
-        DOT_PIXEL.DOT_PIXEL_1)
-    if input.light_level() > 200:
-        LCD1IN8.draw_point(112,
-            108,
-            LCD1IN8.Get_Color(LCD_COLOR.YELLOW),
-            DOT_PIXEL.DOT_PIXEL_4)
-    # Affichage de la temperature
-    LCD1IN8.dis_string(63,
-        61,
-        "" + convert_to_text(heure) + ":" + convert_to_text(minute),
-        LCD1IN8.Get_Color(LCD_COLOR.BLACK))
+    LCD1IN8.dis_string(63, 15, "" + convert_to_text(NPersonnes) + " Personnes", LCD1IN8.Get_Color(LCD_COLOR.BLACK))
+    
+    # Affichage de la luminosité
+    LCD1IN8.dis_string(63, 101, "" + convert_to_text(input.light_level()) + "Lux", LCD1IN8.Get_Color(LCD_COLOR.BLACK))
+    LCD1IN8.draw_rectangle(108, 104, 113, 109, LCD1IN8.Get_Color(LCD_COLOR.BLACK), DRAW_FILL.DRAW_EMPTY, DOT_PIXEL.DOT_PIXEL_1)
+    if input.light_level() > 200 and NPersonnes > 0:  # si 'luninosité' < 200 ET plus que 0 personnes dans la salle, afficher un témoin de lumière.
+        LCD1IN8.draw_point(112, 108, LCD1IN8.Get_Color(LCD_COLOR.YELLOW), DOT_PIXEL.DOT_PIXEL_4)
+    
+    # Affichage de l'heure
+    LCD1IN8.dis_string(63, 61, "" + convert_to_text(heure) + ":" + convert_to_text(minute), LCD1IN8.Get_Color(LCD_COLOR.BLACK))
     LCD1IN8.LCD_Display()
-loops.every_interval(1000, on_every_interval2)
+
+
+# Script start here
+loops.every_interval(10000, on_every_interval2)
+radio.on_received_number(on_received_number)
+loops.every_interval(50000, on_every_interval)
--- main.ts
+++ main.ts
@@ -1,79 +1,57 @@
-radio.onReceivedNumber(function (receivedNumber) {
+//  Init radio transmition
+radio.setGroup(1)
+//  Init LCD_Display
+LCD1IN8.LCD_Init()
+LCD1IN8.LCD_Clear()
+LCD1IN8.LCD_SetBL(10)
+LCD1IN8.LCD_Display()
+//  Init vaiables
+let minute = 34
+let heure = 8
+let NPersonnes = 0
+//  Init functions
+//  Script start here
+loops.everyInterval(10000, function on_every_interval2() {
+    LCD1IN8.LCD_Clear()
+    //  Affichage de la temperature.
+    LCD1IN8.DisString(10, 55, "" + convertToText(input.temperature()) + " C", LCD1IN8.Get_Color(LCD_COLOR.BLACK))
+    if (input.temperature() < 19) {
+        //  si 'temperature' < 19°C, afficher un témoin de chauffe.
+        LCD1IN8.DrawPoint(45, 62, LCD1IN8.Get_Color(LCD_COLOR.RED), DOT_PIXEL.DOT_PIXEL_4)
+    }
+    
+    //  Nombre de personnes dans la salle
+    LCD1IN8.DisString(63, 15, "" + convertToText(NPersonnes) + " Personnes", LCD1IN8.Get_Color(LCD_COLOR.BLACK))
+    //  Affichage de la luminosité
+    LCD1IN8.DisString(63, 101, "" + convertToText(input.lightLevel()) + "Lux", LCD1IN8.Get_Color(LCD_COLOR.BLACK))
+    LCD1IN8.DrawRectangle(108, 104, 113, 109, LCD1IN8.Get_Color(LCD_COLOR.BLACK), DRAW_FILL.DRAW_EMPTY, DOT_PIXEL.DOT_PIXEL_1)
+    if (input.lightLevel() > 200 && NPersonnes > 0) {
+        //  si 'luninosité' < 200 ET plus que 0 personnes dans la salle, afficher un témoin de lumière.
+        LCD1IN8.DrawPoint(112, 108, LCD1IN8.Get_Color(LCD_COLOR.YELLOW), DOT_PIXEL.DOT_PIXEL_4)
+    }
+    
+    //  Affichage de l'heure
+    LCD1IN8.DisString(63, 61, "" + convertToText(heure) + ":" + convertToText(minute), LCD1IN8.Get_Color(LCD_COLOR.BLACK))
+    LCD1IN8.LCD_Display()
+})
+radio.onReceivedNumber(function on_received_number(receivedNumber: number) {
+    
     NPersonnes = receivedNumber
     for (let index = 0; index < 2; index++) {
         led.toggle(0, 0)
         basic.pause(300)
     }
 })
-let NPersonnes = 0
-radio.setGroup(1)
-LCD1IN8.LCD_Init()
-LCD1IN8.LCD_Clear()
-LCD1IN8.LCD_SetBL(10)
-LCD1IN8.LCD_Display()
-let minute = 34
-let heure = 8
-NPersonnes = convertToText(0)
-loops.everyInterval(50000, function () {
+loops.everyInterval(50000, function on_every_interval() {
+    
     minute += 1
     if (minute == 60) {
         heure += 1
+        minute = 0
     }
-})
-loops.everyInterval(10000, function () {
-    LCD1IN8.LCD_Clear()
-    // Affichage de la temperature
-    LCD1IN8.DisString(
-    10,
-    55,
-    "" + convertToText(input.temperature()) + " C",
-    LCD1IN8.Get_Color(LCD_COLOR.BLACK)
-    )
-    if (input.temperature() < 19) {
-        LCD1IN8.DrawPoint(
-        45,
-        62,
-        LCD1IN8.Get_Color(LCD_COLOR.RED),
-        DOT_PIXEL.DOT_PIXEL_4
-        )
+    
+    if (heure == 24) {
+        heure = 0
     }
-    // Nombre de personnes dans la salle
-    LCD1IN8.DisString(
-    63,
-    15,
-    "" + convertToText(NPersonnes) + " Personnes",
-    LCD1IN8.Get_Color(LCD_COLOR.BLACK)
-    )
-    // Affichage de la temperature
-    LCD1IN8.DisString(
-    63,
-    101,
-    "" + convertToText(input.lightLevel()) + "Lux",
-    LCD1IN8.Get_Color(LCD_COLOR.BLACK)
-    )
-    LCD1IN8.DrawRectangle(
-    108,
-    104,
-    113,
-    109,
-    LCD1IN8.Get_Color(LCD_COLOR.BLACK),
-    DRAW_FILL.DRAW_EMPTY,
-    DOT_PIXEL.DOT_PIXEL_1
-    )
-    if (input.lightLevel() > 200 && NPersonnes > 0) {
-        LCD1IN8.DrawPoint(
-        112,
-        108,
-        LCD1IN8.Get_Color(LCD_COLOR.YELLOW),
-        DOT_PIXEL.DOT_PIXEL_4
-        )
-    }
-    // Affichage de la temperature
-    LCD1IN8.DisString(
-    63,
-    61,
-    "" + convertToText(heure) + ":" + convertToText(minute),
-    LCD1IN8.Get_Color(LCD_COLOR.BLACK)
-    )
-    LCD1IN8.LCD_Display()
+    
 })
